23e63698301d79fbc5a204959a505e13c3f49c52
chetan9129 <anku9129@gmail.com>
2023-04-13 16:45:44 +0530

    Deployed Changes

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 // const saltRounds = 10;
 // router.get("/home", (req, res) => {
 //   res.render("/home");
